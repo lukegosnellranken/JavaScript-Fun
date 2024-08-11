44f36717b11dda5d6fe25b59ffa41af7b88e6fea
Luke Gosnell <lukegosnell324@gmail.com>
2024-08-10 19:45:23 -0500

    node installed, page 3 project complete

---
+++ script.js
@@ -0,0 +1,21 @@
+// Page 3
+
+let x = 10;
+let y = 0;
+
+for (let i = 0; i < 10; i++) {
+    y = y + x;
+    x--;
+}
+
+console.log(y);
+
+// Book solution on page 5
+
+let total = 0, count = 1;
+while (count <= 10) {
+    total += count;
+    count++;
+}
+console.log(total);
+
