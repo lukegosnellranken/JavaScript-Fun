3b8f3f2ed0abdf0eaeae25718c8d0e3768d6933a
Luke Gosnell <lukegosnell324@gmail.com>
2025-02-01 11:33:06 -0600

    2/1/25 commit

--- script.js
+++ script.js
@@ -50,25 +50,65 @@
 //     }
 // }
 
-// Page 38: Chessboard
-let size = 12;
-let string = '';
-for (o = 0; o < size; o++) {
-    for (i = 0; i < size; i++) {
-        if (o % 2 == 0) {
-            if (i % 2 == 0) {
-                string += '#';
-            } else {
-                string += ' '
-            }
-        } else {
-            if (i % 2 == 0) {
-                string += ' ';
-            } else {
-                string += '#'
-            }
-        }
-    }
-    string += `\n`;
+// // Page 38: Chessboard
+// let size = 12;
+// let string = '';
+// for (o = 0; o < size; o++) {
+//     for (i = 0; i < size; i++) {
+//         if (o % 2 == 0) {
+//             if (i % 2 == 0) {
+//                 string += '#';
+//             } else {
+//                 string += ' '
+//             }
+//         } else {
+//             if (i % 2 == 0) {
+//                 string += ' ';
+//             } else {
+//                 string += '#'
+//             }
+//         }
+//     }
+//     string += `\n`;
+// }
+// console.log(string);
+
+// // Page 44
+// const chicken = () => {
+//     return egg();
+// }
+
+// const egg = () => {
+//     return chicken();
+// }
+
+// console.log(chicken());
+
+// // Page 45
+// const roundTo = (a, b = 1) => {
+//     let remainder = a % b;
+//     return a - remainder + (remainder < b / 2 ? 0 : b); 
+// }
+// console.log(roundTo(4.5));
+// console.log(roundTo(4.5,2));
+
+// // null and undefined both indicate a lack of meaningful value. However, they are still different types.
+// if (null == undefined) {
+//     console.log("hello");
+// }
+
+// // Will return the left value if it evaluates to true, otherwise the right value
+// console.log(false || NaN);
+
+// // Will return the left value if it evaluates to false, otherwise the right value
+// console.log(false && NaN);
+
+function multiplier(factor) {
+    // The below function is a closure (factor is the local parameter. In this example's case: 2)
+    return number => number * factor;
 }
-console.log(string);
+// The below statement creates an environment in which there is a function where factor is equal to 2.
+let twice = multiplier(2);
+// The below statement passes in the argument of 5 to twice, which is grasping a function where the parameter is multiplied by 2.
+console.log(twice(5));
+
